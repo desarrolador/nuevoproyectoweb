0e557297d6ad2ac6d9d0d616ada29db33878bf7e
miguel <miguelrusso2840@gmail.com>
2025-06-06 16:25:16 -0300

    guardo actualizaciones

---
+++ datosdelsocio.js
@@ -0,0 +1,67 @@
+{
+  "users": [
+    {
+      "partner_user_id": "eXiiAD13o99",
+      "user_id": "aksdjadja123123",
+      "phone": "55 61991049887",
+      "first_name": "Bruno",
+      "last_name": "Gimenes",
+      "country": "BR",
+      "document_type": "CPF",
+      "document_number": "12345678910",
+      "status": "OK",
+      "accounts": [
+        {
+          "account_id": "kdkdkd919191",
+          "currency": "USD",
+          "balance": 200
+        },
+        {
+          "account_id": "klkasd213",
+          "currency": "EUR",
+          "balance": 10
+        }
+      ],
+      "cards": [
+        {
+          "card_id": "99999",
+          "number_token": "tok_1234678",
+          "cvv_token": "tok_987655",
+          "expiration_date": "01/30",
+          "network": "VISA",
+          "last_digits": "5555",
+          "card_holder": "Bruno Gimenes",
+          "status": "ACTIVE"
+        },
+        {
+          "card_id": "8888",
+          "number_token": "tok_9876",
+          "cvv_token": "tok_4567",
+          "expiration_date": "02/31",
+          "network": "MASTERCARD",
+          "last_digits": "3333",
+          "card_holder": "Bruno Gimenes",
+          "status": "ORDERED"
+        }
+      ]
+    },
+    {
+      "partner_user_id": "exHnkpo987",
+      "user_id": "hjkasd123",
+      "phone": "55 123456789",
+      "first_name": "Marcelo",
+      "last_name": "Dos Santos",
+      "country": "BR",
+      "document_type": "CPF",
+      "document_number": "098754123",
+      "status": "OK",
+      "accounts": [
+        {
+          "account_id": "vbnm234567",
+          "currency": "GBP",
+          "balance": 15
+        }
+      ]
+    }
+  ]
+}
